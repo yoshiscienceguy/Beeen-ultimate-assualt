88d71d0dad56d719758dbfcfe469b169fd632095
WATERMELONPOP <f.depaz@hotmail.com>
2022-12-27 19:35:50 -0800

    updated lobby choices

--- Assets/Fern Stuff/Scripts/_Scripts/Lobby/LobbyRoomPanel.cs
+++ Assets/Fern Stuff/Scripts/_Scripts/Lobby/LobbyRoomPanel.cs
@@ -4,10 +4,9 @@ using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class LobbyRoomPanel : MonoBehaviour {
-    [SerializeField] private float _difficultyDialMaxAngle = 100f;
 
-    [SerializeField] private TMP_Text _nameText, _typeText, _playerCountText;
-    [SerializeField] private Transform _difficultyMeter;
+    [SerializeField] private TMP_Text _nameText, _playerCountText;
+
 
     public Lobby Lobby { get; private set; }
 
@@ -20,16 +19,11 @@ public class LobbyRoomPanel : MonoBehaviour {
     public void UpdateDetails(Lobby lobby) {
         Lobby = lobby;
         _nameText.text = lobby.Name;
-        _typeText.text = Constants.GameTypes[GetValue(Constants.GameTypeKey)];
-
-        var point = Mathf.InverseLerp(0, Constants.Difficulties.Count - 1, GetValue(Constants.DifficultyKey));
-        _difficultyMeter.transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(_difficultyDialMaxAngle, -_difficultyDialMaxAngle, point));
+        
 
         _playerCountText.text = $"{lobby.Players.Count}/{lobby.MaxPlayers}";
 
-        int GetValue(string key) {
-            return int.Parse(lobby.Data[key].Value);
-        }
+
     }
 
     public void Clicked() {
--- Assets/Fern Stuff/Scripts/_Scripts/Models/Constants.cs
+++ Assets/Fern Stuff/Scripts/_Scripts/Models/Constants.cs
@@ -5,6 +5,4 @@ public class Constants {
     public const string DifficultyKey = "d";
     public const string GameTypeKey = "t";
 
-    public static readonly List<string> GameTypes = new() { "Battle Royal", "Team Death Match"};
-    public static readonly List<string> Difficulties = new() { "This box is currently usless", "why did you click it it is clearly usless" };
 }
