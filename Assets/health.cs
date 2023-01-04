1eda279428a3da19d620f2a76a923d08a56969f1
WATERMELONPOP <f.depaz@hotmail.com>
2023-01-03 19:32:24 -0800

    health works kinda

--- Assets/health.cs
+++ Assets/health.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class health : MonoBehaviour
+using Unity.Netcode;
+using UnityEngine.UI;
+public class health : NetworkBehaviour
 {
     public float maxhealth = 21;
+    public NetworkVariable<float> netHealth;
     private float currenthealth;
+    public Image healthbar;
     // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
         currenthealth = maxhealth;
+        netHealth = new NetworkVariable<float>();
+
+            netHealth.Value = currenthealth;
+
+        if (!IsOwner) {
+            return;
+        }
         
+        healthbar = GameObject.Find("Foreground_Healthbar").GetComponent<Image>();
+        
+       
     }
+   
 
     // Update is called once per frame
     void Update()
@@ -22,7 +36,13 @@ public class health : MonoBehaviour
 
     public void takedamage(float damage)
     {
+        netHealth.Value -= damage;
         currenthealth -= damage;
+        if (IsOwner)
+        {
+            healthbar = GameObject.Find("Foreground_Healthbar").GetComponent<Image>();
+            healthbar.fillAmount = netHealth.Value / maxhealth;
+        }
         if(currenthealth <= 0)
         {
             //oof
