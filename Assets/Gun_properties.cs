f4e3f89d058316fc5959c1574f03e5c5b1a6e037
WATERMELONPOP <f.depaz@hotmail.com>
2023-01-13 19:22:14 -0800

    added shotgun

---
+++ Assets/Gun_properties.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rarity
+{
+    good, normal, meh
+}
+public class Gun_properties : MonoBehaviour
+{
+    public float ShootingFrequency = 0.5;
+    public float bulletAmount = 1;
+    public float maxDamage = 10;
+    public float minimumDamage = 1;
+    public bool raycasting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
